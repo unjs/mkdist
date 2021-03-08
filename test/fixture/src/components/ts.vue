aa15aa55b2e1036a1857665a6e2f7f8cdc41b9aa
Daniel Roe <daniel@roe.dev>
2021-03-08 20:26:57 +0000

    test: fix syntax

--- test/fixture/src/components/ts.vue
+++ test/fixture/src/components/ts.vue
@@ -3,11 +3,14 @@
 </template>
 
 <script lang="ts">
-import { test } from '..'
+import test from '..'
+
+const str: 'test' = 'test'
 
 export default {
-  data () {
-    test: test() as () => any
-  }
+  data: () => ({
+    test: test(),
+    str
+  })
 }
 </script>
