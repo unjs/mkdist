c37fdb65ae740dc7b22623634dc5f6aae98a4595
Pooya Parsa <pooya@pi0.io>
2022-10-27 12:41:37 +0200

    fix: node 13 support

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,9 +1,16 @@
-import { pipeline } from 'stream/promises'
+import { pipeline } from 'stream'
 import { createReadStream, createWriteStream } from 'fs'
 
 export function copyFileWithStream (srcPath: string, outPath: string) {
   const srcStream = createReadStream(srcPath)
   const outStream = createWriteStream(outPath)
-
-  return pipeline(srcStream, outStream)
+  return new Promise((resolve, reject) => {
+    pipeline(srcStream, outStream, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(undefined)
+      }
+    })
+  })
 }
