3ae59529a222ec498909202d20138ccab8183735
Daniel Roe <daniel@roe.dev>
2023-03-07 17:45:01 +0000

    test: update quotes in test

--- test/index.test.ts
+++ test/index.test.ts
@@ -107,7 +107,7 @@ describe("mkdist", () => {
 
     expect(
       await readFile(resolve(rootDir, "dist/star/index.d.ts"), "utf8")
-    ).toMatch("export * from './other.js';\n");
+    ).toMatch('export * from "./other.js";\n');
     expect(
       await readFile(resolve(rootDir, "dist/bar/esm.d.mts"), "utf8")
     ).toMatch("declare");
