0cff10baf1b93232ccee544ec9d856e3fef2fc19
liuyang0826 <1161906592@qq.com>
2023-11-22 21:23:48 +0800

    fix: resolve dynamic import paths (#165)

--- test/fixture/src/index.ts
+++ test/fixture/src/index.ts
@@ -5,4 +5,7 @@ const foo: string = "foo";
 
 export const importFoo = () => import("node:path");
 
+export const dynamicLibImport = () => import("node:module");
+export const dynamicImport = () => import("./bar");
+
 export default () => foo + bar;
