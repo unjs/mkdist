7ef3d73ec9cc318842f9b7650ae9b2c467d2d253
XiNiHa <me@xiniha.dev>
2024-05-29 01:28:44 +0900

    feat: add declarationExt option

--- src/loaders/vue.ts
+++ src/loaders/vue.ts
@@ -114,5 +114,5 @@ const scriptLoader = vueBlockLoader({
   outputLang: "js",
   type: "script",
   exclude: [/\bsetup\b/],
-  validExtensions: [".js", ".mjs"],
+  validExtensions: [".js", ".mjs", ".ts", ".mts"],
 });
