bdd83dd878b00255b8d990f8138a09e6134ddde3
Teages <Teages@teages.xyz>
2025-01-18 20:58:36 +0800

    test: add test for transpile vue template

--- test/vue-template.test.ts
+++ test/vue-template.test.ts
@@ -4,19 +4,104 @@ import { transpileVueTemplate } from "../src/utils/vue";
 import { createRequire } from "node:module";
 
 describe("vue template", () => {
-  it("simple", async () => {
-    expect(await fixture(`<div>{{ data!.test }}</div>`)).toEqual(
-      `<div>{{ data.test }}</div>`,
-    );
-  });
-
   it("v-for", async () => {
     expect(
       await fixture(
-        `<div v-for="(item as string, index) in items as unknown[]" :key="item" :index>item</div>`,
+        `<div v-for="item as string in items as unknown[]" :key="item">{{ item }}</div>`,
+      ),
+    ).toEqual(`<div v-for="item in items" :key="item">{{ item }}</div>`);
+    expect(
+      await fixture(
+        `<div v-for="(item as string, index) in items as unknown[]" :key="item" :index>{{ item }}</div>`,
       ),
     ).toEqual(
-      `<div v-for="(item, index) in items" :key="item" :index>item</div>`,
+      `<div v-for="(item, index) in items" :key="item" :index>{{ item }}</div>`,
+    );
+  });
+
+  it("v-if", async () => {
+    expect(await fixture(`<div v-if="(data as any).test" />`)).toEqual(
+      `<div v-if="data.test" />`,
+    );
+  });
+
+  it("v-show", async () => {
+    expect(await fixture(`<div v-show="(data as any).show" />`)).toEqual(
+      `<div v-show="data.show" />`,
+    );
+  });
+
+  it("v-model", async () => {
+    expect(await fixture(`<input v-model="(data as string)" />`)).toEqual(
+      `<input v-model="data" />`,
+    );
+  });
+
+  it("v-on", async () => {
+    expect(await fixture(`<div @click="handleClick as () => void" />`)).toEqual(
+      `<div @click="handleClick" />`,
+    );
+    expect(await fixture(`<div @click="handleClick()" />`)).toEqual(
+      `<div @click="handleClick()" />`,
+    );
+    expect(
+      await fixture(
+        `<div @click="(e: unknown) => handleClick(e as MouseEvent)" />`,
+      ),
+    ).toEqual(`<div @click="(e) => handleClick(e)" />`);
+  });
+
+  it("custom directives", async () => {
+    expect(
+      await fixture(`<div v-highlight="(highlight as boolean)" />`),
+    ).toEqual(`<div v-highlight="highlight" />`);
+  });
+
+  it("v-bind", async () => {
+    expect(await fixture(`<div v-bind="(props as any)" />`)).toEqual(
+      `<div v-bind="props" />`,
+    );
+    expect(
+      await fixture(`<div :key="(value as any)" data-test="test" />`),
+    ).toEqual(`<div :key="value" data-test="test" />`);
+  });
+
+  it("interpolation", async () => {
+    expect(await fixture(`<div>{{ data!.test }}</div>`)).toEqual(
+      `<div>{{ data.test }}</div>`,
+    );
+    expect(await fixture(`<div>hi {{ data!.test }}</div>`)).toEqual(
+      `<div>hi {{ data.test }}</div>`,
+    );
+    expect(
+      await fixture(
+        `<div>{{ typeof data!.test === "string" ? data!.test : getKey(data!.test) }}</div>`,
+      ),
+    ).toEqual(
+      `<div>{{ typeof data.test === "string" ? data.test : getKey(data.test) }}</div>`,
+    );
+  });
+
+  it("keep comments", async () => {
+    expect(
+      await fixture(`<div>{{ data!.test }}</div><!-- comment -->`),
+    ).toEqual(`<div>{{ data.test }}</div><!-- comment -->`);
+  });
+
+  it("keep text", async () => {
+    expect(await fixture(`<div>data!.test</div>`)).toEqual(
+      `<div>data!.test</div>`,
+    );
+  });
+
+  it("keep empty", async () => {
+    expect(await fixture(`<div>{{}}</div>`)).toEqual(`<div>{{}}</div>`);
+    expect(await fixture(`<div @click="" />`)).toEqual(`<div @click="" />`);
+  });
+
+  it("keep error", async () => {
+    expect(await fixture(`<div>{{ data. }}</div>`)).toEqual(
+      `<div>{{ data. }}</div>`,
     );
   });
 
